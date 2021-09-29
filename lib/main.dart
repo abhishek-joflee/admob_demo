f60b34013d078886f39aff6e604168347436a59a
Abhishek Patil <abhishek.patil@joflee.com>
2021-09-29 10:32:02 +0530

    some change

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 //? my-imports
+import 'app_open_ad_manager.dart';
 import 'homepage_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await MobileAds.instance.initialize();
+  AppOpenAdManager appOpenAdManager = AppOpenAdManager();
+  appOpenAdManager.showAdIfAvailable();
   runApp(const MyApp());
 }
 
