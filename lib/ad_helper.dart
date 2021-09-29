d838e7a2f7d02d775a45561d33c84247714f60af
Abhishek Patil <abhishek.patil@joflee.com>
2021-09-29 16:59:43 +0530

    unitID added

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -11,6 +11,16 @@ class AdHelper {
     }
   }
 
+  static String get nativeAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/2247696110';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3940256099942544/3986624511';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
       return "ca-app-pub-3940256099942544/1033173712";
